228a4cb01733f811765d65cc15e8a195b4fa89ce
Terria028 <86240437+Terria-K@users.noreply.github.com>
2023-03-02 19:18:11 +0800

    Hide Window Console

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,17 @@
 #define SDL_MAIN_HANDLED
 
+#if WINDOWS
+#include <Windows.h>
+#endif
 #include "game.h"
 #include <memory>
 
 int main(int argc, char* argv[]) {
-    std::unique_ptr<Game> game = std::make_unique<Game>();
+#if WINDOWS
+    ShowWindow(GetConsoleWindow(), SW_HIDE);
+#endif
+
+    auto game = std::make_unique<Game>();
     game->run("Ping_Pong", 640, 320, 0);
 
     return 0;
